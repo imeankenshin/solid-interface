93a77e73fed7dd6656258c57d0c5a42bf22ac49a
LinoRino <omurakenshin@gmai.com>
2023-07-30 13:53:33 +0900

    add components generator

--- turbo/generators/config.ts
+++ turbo/generators/config.ts
@@ -1,5 +1,13 @@
 import type { PlopTypes } from "@turbo/gen"
 
+function add(path: string, templateFile: string): PlopTypes.ActionType {
+  return {
+    type: "add",
+    path: `packages/components/{{name}}/${path}`,
+    templateFile: `templates/${templateFile}.hbs`,
+  }
+}
+
 export default function generator(plop: PlopTypes.NodePlopAPI): void {
   // create a generator
   plop.setGenerator("component", {
@@ -9,17 +17,21 @@ export default function generator(plop: PlopTypes.NodePlopAPI): void {
       {
         type: "input",
         name: "name",
-        message: "Component name",
+        message: "Component name:",
+      },
+      {
+        type: "input",
+        name: "description",
+        message: "Component description:",
       },
     ],
     // perform actions based on the prompts
     actions: [
-      {
-        type: "add",
-        path: "packages/components/turbo/src/{{name}}/{{name}}.tsx",
-        templateFile:
-          "packages/components/turbo/generator/templates/component.tsx.hbs",
-      },
+      add("src/index.ts", "index.ts"),
+      add("src/{{name}}.tsx", "component.tsx"),
+      add("package.json", "package.json"),
+      add("README.md", "README.md"),
+      add("tsconfig.json", "tsconfig.json"),
     ],
   })
 }
