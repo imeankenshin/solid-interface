b08afe483624911dc0a3ccbc0bd086f35ce8feb4
LinoRino <omurakenshin@gmai.com>
2023-10-08 08:58:01 +0900

    configure storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,28 +1,25 @@
 import type { StorybookConfig } from "storybook-solidjs-vite"
-import fs from "node:fs"
 
-function getStories({ dir = "components" }) {
-  const dirName = `packages/${dir}`
-  const scope = fs.readdirSync(dirName)
-  return scope
-    .map((pkg) => `${dirName}/${pkg}/stories`)
-    .filter((storyDir) => fs.existsSync(storyDir))
-    .map((storyDir) => `../${storyDir}/*.stories.tsx`)
+import { join, dirname } from "path"
+
+/**
+ * This function is used to resolve the absolute path of a package.
+ * It is needed in projects that use Yarn PnP or are set up within a monorepo.
+ */
+function getAbsolutePath(value: string): any {
+  return dirname(require.resolve(join(value, "package.json")))
 }
-
 const config: StorybookConfig = {
-  stories: [
-    ...getStories({ dir: "components" }),
-    "../stories/*.{stories.tsx,mdx}",
-  ],
+  stories: ["../packages/**/stories/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/addon-styling",
-    "@storybook/addon-a11y",
+    getAbsolutePath("@storybook/addon-links"),
+    getAbsolutePath("@storybook/addon-essentials"),
+    getAbsolutePath("@storybook/addon-interactions"),
   ],
-  framework: "storybook-solidjs-vite",
+  framework: {
+    name: getAbsolutePath("storybook-solidjs-vite"),
+    options: {},
+  },
   docs: {
     autodocs: "tag",
   },
